ebade9e2819b02a3f08961abdcf09666dc96c26a
willhcurry <willhcurry@gmail.com>
2024-08-11 19:28:26 -0600

    banner component, fonts, and other css

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Banner from '@/components/banner.client';
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-purple-951">
-      <h1>Coffee Connoisseur</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-14">
+      <Banner />
     </main>
   );
 }
