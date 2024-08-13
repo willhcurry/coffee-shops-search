a87572713f23ad2403e40f79ee1a6b2cc76da658
willhcurry <willhcurry@gmail.com>
2024-08-13 17:24:37 -0600

    Card component and dynamic coffee store

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Banner from '@/components/banner.client';
-import Image from "next/image";
+import Card from '@/components/card.server';
 
 export default function Home() {
+  const coffeeStoreId = 'espresso-coffee'
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-14">
-      <Banner />
-    </main>
+    <div className="mb-56">
+      <main className="flex min-h-screen flex-col items-center justify-between p-14">
+        <Banner />
+        <Card
+          name="Dark Horse Coffee"
+          imgUrl='/static/hero-image.png'
+          href={`/coffee-store/${coffeeStoreId}`}
+        />
+      </main>
+    </div>
   );
 }
