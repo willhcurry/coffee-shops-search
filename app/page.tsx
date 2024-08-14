857e29fef55245fc551ca92a6ecc9418dc57ffee
willhcurry <willhcurry@gmail.com>
2024-08-13 19:30:22 -0600

    grid setup, styling, minr changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,53 @@ import Banner from '@/components/banner.client';
 import Card from '@/components/card.server';
 
 export default function Home() {
-  const coffeeStoreId = 'espresso-coffee'
+  const coffeeStoreId = 'espresso-coffee';
+  const coffeeStores = [
+    {
+      "name": "Dark Horse Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "name": "Dark Horse Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "name": "Dark Horse Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "name": "Dark Horse Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "name": "Dark Horse Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "name": "Dark Horse Coffee",
+      "imgUrl": "https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+  ];
   return (
     <div className="mb-56">
       <main className="flex min-h-screen flex-col items-center justify-between p-14">
         <Banner />
-        <Card
-          name="Dark Horse Coffee"
-          imgUrl='/static/hero-image.png'
-          href={`/coffee-store/${coffeeStoreId}`}
-        />
+
+        <div className='mt-20'>
+          <h2 className='mt-8 pb-8 text-4xl font-bold text-white'>
+            Utah Stores
+          </h2>
+          <div className='grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6'>
+            {coffeeStores.map((coffeeStore, idx) => (
+              <Card
+                key={`${coffeeStore.name}-${idx}`}
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${idx}`}
+              />
+            ))}
+          </div>
+        </div>
       </main>
     </div>
   );
